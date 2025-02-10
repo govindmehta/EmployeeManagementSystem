9bef8e1b6fb1c25ad86179ffa144ee32a0f56151
Govind Mehta <govindmehta.gov@gmail.com>
2025-02-11 00:20:40 +0530

    basic setup done

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.js";
-import attendanceRoutes from "./routes/attendance.js";
-import salaryRoutes from "./routes/salary.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/users", userRoutes);
-app.use("/api/attendance", attendanceRoutes);
-app.use("/api/salary", salaryRoutes);
 
 
 const PORT = process.env.PORT || 5000;
